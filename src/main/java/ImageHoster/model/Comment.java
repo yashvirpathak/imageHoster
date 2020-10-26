e06a59affe61f97c9616aa81e2abc87f88507124
Pathak, Yashvir <yashvir.pathak@intel.com>
2020-10-26 23:18:49 +0530

    Comment model created

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,89 @@
+package ImageHoster.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+//@Entity annotation specifies that the corresponding class is a JPA entity
+@Entity
+//@Table annotation provides more options to customize the mapping.
+//Here the name of the table to be created in the database is explicitly mentioned as 'users'. Hence the table named 'users' will be created in the database with all the columns mapped to all the attributes in 'User' class
+@Table(name = "comments")
+public class Comment {
+    //@Id annotation specifies that the corresponding attribute is a primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column annotation specifies that the attribute will be mapped to the column in the database.
+    //Here the column name is explicitly mentioned as 'id'
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "date")
+    private Date createdDate ;
+
+    //The 'comments' table is mapped to 'users' table with Many:One mapping
+    //One comment can have only one user (owner) but one user can have multiple comments
+    //FetchType is EAGER
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comments' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "user")
+    private User user;
+
+    //The 'comments' table is mapped to 'images' table with Many:One mapping
+    //One image can have multiple comments but one comment can only belong to one image.
+    //FetchType is EAGER
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comments' table referring the primary key in 'images' table will be 'image'
+    @JoinColumn(name = "image")
+    private Image image;
+
+    public Comment(){}
+
+    public Comment(int id, String text, Date date) {
+        this.setId(id);
+        this.setText(text);
+        this.setCreatedDate(date);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
